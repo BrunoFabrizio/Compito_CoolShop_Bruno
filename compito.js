e5714e837db012bf93b803c470c4eec5ae7baeaf
BrunoFabrizio <113614047+BrunoFabrizio@users.noreply.github.com>
2022-12-06 00:25:21 +0100

    Add files via upload

--- compito.js
+++ compito.js
@@ -1,18 +1,36 @@
-const inputconsole = process.argv;
+const consoleinput = process.argv;
+
 
 
 const fs = require("fs");
 const readline = require("readline");
-const stream = fs.createReadStream("./Coolshoop.csv");
-const rl = readline.createInterface({ input: stream });
+const stream = fs.createReadStream(consoleinput[2]);
+const read = readline.createInterface({ input: stream });
 let data = [];
-
-rl.on("line", (row) => {
-data.push(row.split(","));
+let collum = consoleinput[3];
+let key = consoleinput[4];
+let flag = false;
+read.on("line", (row) => {
+    data.push(row.split(","));
 });
 
-rl.on("close", () => {
-console.log(data);
+read.on("close", () => {
+
+    for(let i = 0; i < data.length; i++)
+    {
+        if(data[i][collum]===key)
+        {
+            console.log(data[i]);
+            flag = true;
+        }
+    }
+    if(flag == false)
+    {
+        console.log("Chiave o attributo sbagliato");
+    }
 });
 
-console.log("Hi")
+console.log(consoleinput[3]);
+console.log(consoleinput[4]);
+
+
