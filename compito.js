ce07d1eb0eb89b16858b85d1e2d99393f4278356
BrunoFabrizio <fabriziobrunop@gmail.com>
2022-12-05 17:24:14 +0100

    first commit

---
+++ compito.js
@@ -0,0 +1,18 @@
+const inputconsole = process.argv;
+
+
+const fs = require("fs");
+const readline = require("readline");
+const stream = fs.createReadStream("./Coolshoop.csv");
+const rl = readline.createInterface({ input: stream });
+let data = [];
+
+rl.on("line", (row) => {
+data.push(row.split(","));
+});
+
+rl.on("close", () => {
+console.log(data);
+});
+
+console.log("Hi")
